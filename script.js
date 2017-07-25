18bef36cb40230e3d05a0b056ca4d80dd61c7cf2
nital patel <patelnital36@gmail.com>
2017-07-25 12:24:23 -0400

    updated files

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-// javascript to be added
+// javascript to be addedvar
+var mail = function function_name(argument) {
+  prompt("join in mailing system");
+}
+  document.getElementById("button-yellow").addEventListener("click" mail);
